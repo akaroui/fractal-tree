e24359d1eba2ac2010d43ca64a2124184dcf9b1e
gaetan <gaetan.desrues@inria.fr>
2021-08-20 03:09:07 +0200

    update FractalTree package

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="FractalTree",
-    version="0.0.007",
+    version="0.0.009",
     author="Gaetan Desrues",
     author_email="gaetan.desrues@inria.fr",
     url="https://github.com/GaetanDesrues/fractal-tree",
-    packages=find_packages(),
+    packages=["FractalTree"],
 )
