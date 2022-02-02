6460519d4499fe1183ffee9e321ca5c442659804
Gaetan Desrues <gaetan.desrues@inria.fr>
2022-02-02 23:24:59 +0100

    ok

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+from Cython.Build import cythonize
 from setuptools import setup
 
-cython_compile = True
-if cython_compile:
-    from build_geodesic import setup_geo
-
-    setup_geo()
+# cython_compile = True
+# if cython_compile:
+#     from build_geodesic import setup_geo
+#
+#     setup_geo()
 
 setup(
     name="FractalTree",
-    version="0.0.44",
+    version="0.0.45",
     url="https://github.com/GaetanDesrues/fractal-tree",
     packages=["FractalTree"],
     include_package_data=True,
     package_data={
         "FractalTree/core": ["cla.h", "cla.pxd", "rect.pyx"],
     },
+    ext_modules=cythonize(
+        "FractalTree/core/rect.pyx",
+        compiler_directives={"language_level": "3"},
+    ),
 )
