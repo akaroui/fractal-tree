110be3135ec7ca1bad5e74e02d03cf7a49abdf4d
Gaetan Desrues <gaetan.desrues@inria.fr>
2022-02-02 23:05:11 +0100

    new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if cython_compile:
 
 setup(
     name="FractalTree",
-    version="0.0.39",
+    version="0.0.41",
     url="https://github.com/GaetanDesrues/fractal-tree",
     packages=["FractalTree"],
 )
