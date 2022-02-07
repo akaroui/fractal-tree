c7aae9d7e9ea756130cf4e45edc6771da8538a49
AKAROUI <akaroui@MacBook-Pro-de-inHEART172.local>
2022-02-07 14:21:53 +0100

    modify core/setup.py to include numpy dir

--- FractalTree/core/setup.py
+++ FractalTree/core/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 # python setup.py build_ext --inplace
 
-setup(ext_modules=cythonize("rect.pyx"))
+setup(ext_modules=cythonize("rect.pyx"),include_dirs=[numpy.get_include()])
